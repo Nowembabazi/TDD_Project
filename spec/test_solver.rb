b4a2d03ac3de7fbb41d18ae6c918a9695b2e60c2
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-01-26 10:15:23 +0100

    Update test_solver.rb file by adding more unit tests

--- spec/test_solver.rb
+++ spec/test_solver.rb
@@ -43,3 +43,24 @@ describe Solver do
     end
   end
 end
+
+describe 'Reverse' do
+  it 'returns the reverse of a word' do
+    expect(@solver.reverse('hello')).to eq('olleh')
+  end
+end
+describe 'fizzbuzz' do
+  it 'should return fizzbuzz when number is divisible by 3 and 5' do
+    expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+  end
+  it 'Should return fizz  when number is divisible by 3' do
+    expect(@solver.fizzbuzz(9)).to eq('fizz')
+  end
+  it 'should return Buzz if number is divisible by 5' do
+    expect(@solver.fizzbuzz(5)).to eq('buzz')
+  end
+  it 'should return number if number is not divisible by 3 or 5' do
+    expect(@solver.fizzbuzz(7)).to eq('7')
+  end
+end
+end
