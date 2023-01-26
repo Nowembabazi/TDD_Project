9f2f494e9bbfc64186623c0036da41dbf2cd8c76
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-01-26 10:32:57 +0300

    Add factorial function

--- solver.rb
+++ solver.rb
@@ -8,20 +8,3 @@ class Solver
       num * factorial(num - 1)
     end
   end
-
-  def reverse(word)
-    word.reverse
-  end
-
-  def fizzbuzz(number)
-    if (number % 3).zero? && (number % 5).zero?
-      'fizzbuzz'
-    elsif (number % 3).zero?
-      'fizz'
-    elsif (number % 5).zero?
-      'buzz'
-    else
-      number.to_s
-    end
-  end
-end
