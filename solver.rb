0e737ccbd0510ae3cec0f7ddc196ca1ed9547c7f
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-01-26 15:00:08 +0300

    iniatial factorials

--- solver.rb
+++ solver.rb
@@ -12,16 +12,16 @@ class Solver
   def reverse(word)
     word.reverse
   end
-  
- def fizzbuzz(number)
-  if (number % 3).zero? && (number % 5).zero?
-    'fizzbuzz'
-  elsif (number % 3).zero?
-    'fizz'
+
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
   elsif (number % 5).zero?
-    'buzz'
+      'buzz'
   else
-    number.to_s
+      number.to_s
   end
  end
 end
